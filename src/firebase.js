ff7122e8981443ccd14756ed80918462a2bd4a02
YunusKaratepe <yunus.karatepe1699@gmail.com>
2021-01-26 10:59:44 +0300

    firabase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCYAGe7lUKt__9GAUWXLXHZsZM0pCuZxoQ",
-    authDomain: "facebook-messenger-clone-7340f.firebaseapp.com",
-    projectId: "facebook-messenger-clone-7340f",
-    storageBucket: "facebook-messenger-clone-7340f.appspot.com",
-    messagingSenderId: "516242361111",
-    appId: "1:516242361111:web:6aa2c2a9b713e8b7d64552",
-    measurementId: "G-X56LXR3JZX"
-};
+const firebaseConfig = // here you should have your firebase config object;
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
