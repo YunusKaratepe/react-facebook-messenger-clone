06fcefd2331da8f5ad1afbe934e2e2d2ec61f274
YunusKaratepe <yunus.karatepe1699@gmail.com>
2021-01-27 18:22:59 +0300

    sleep sugar

--- src/Message.js
+++ src/Message.js
@@ -3,14 +3,32 @@ import {Card, CardContent, Typography} from '@material-ui/core';
 import './Message.css';
 
 const Message = forwardRef(({message, username}, ref) => {
+
+
     return (
         <div ref={ref} className={`${ username === message.username ? "userMessageDiv" : "messageDiv" }`}>
             <Card className={`${ username === message.username ? "userCard" : "card" }`}>
                 <CardContent>
-                    <Typography color="textSecondary">
+                    <Typography style={{fontSize: "12px", float: "left"}} color="textSecondary">
                         {message.username}
                     </Typography>
-                    <Typography>
+
+                    <Typography style={{fontSize: "12px", float: "right", marginLeft: "4px"}} color="textSecondary">
+                        {`${ message.timestamp ? 
+                            new Date(message.timestamp.seconds * 1000).getHours() + ':' + 
+                            (
+                                new Date(message.timestamp.seconds * 1000).getMinutes() <= 10 ? 
+                                '0' + new Date(message.timestamp.seconds * 1000).getMinutes(): 
+                                '' + new Date(message.timestamp.seconds * 1000).getMinutes()
+                            )
+                        : ''
+                        }`}
+
+                    </Typography>
+
+                    
+            
+                    <Typography style={{clear: "left"}}>
                         {message.message}
                     </Typography>
                 </CardContent>
